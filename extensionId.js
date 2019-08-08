0b60c2022a460aa147830d5e0d47402d196abbe8
lolamtisch <francisco.seipel@yahoo.de>
2019-08-08 18:27:26 +0200

    [TASK] Add Soundcloud

---
+++ extensionId.js
@@ -0,0 +1,4 @@
+var extensionId = "londahcleefkodmnlammpkcdjekmmafj"; //Chrome
+if(typeof browser !== 'undefined' && typeof chrome !== "undefined"){
+  extensionId = "1cf038ecb5fc74f2a6ca0811c2c14d89fc230b89@temporary-addon"; //Firefox
+}
